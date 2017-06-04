3bf00d86ba48975e107edbacbcd4d4bf0a7b158f
Moises <moycs777@gmail.com>
2017-06-04 17:30:25 -0400

    scrapper, solo faltan detalles de hoteles

--- aplication/scrapper.php
+++ aplication/scrapper.php
@@ -10,6 +10,18 @@
 	if ($conexion->connect_errno) {
 	    echo "Falló la conexión con MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
 	}	
+	$sql = "TRUNCATE ciudades";
+	if ($conexion->query($sql) === TRUE) {
+	    echo "Record deleted successfully";
+	} else {
+	    echo "Error deleting record: " . $conexion->error;
+	}
+	$sql = "TRUNCATE hoteles";
+	if ($conexion->query($sql) === TRUE) {
+	    echo "Record deleted successfully";
+	} else {
+	    echo "Error deleting record: " . $conexion->error;
+	}
 
 	$uri = "https://www.tripadvisor.com.ve/";
 
@@ -29,44 +41,71 @@
 	echo "<pre>";
 	print_r ($ciudad_enlace);
 	echo "</pre>";
-	$j = 0;
-
-
-	//creacion de ciudades
+	$j = 0;	
 	
-	//echo "Ciudades grabadas";
 	for ($i=0; $i < 3; $i++) { 
-		
+
+		$ciudad = $ciudad_enlace['ciudades'][$i]; 
+		$iterador = floor ( preg_replace("/[^0-9]/", "", $ciudad)/50 )-1;  
+		/*echo "Iterador :". $iterador . "<br>";*/
 		mysqli_query($conexion,"insert into ciudades (nombre, enlace) values 
 		                       ('".$ciudad_enlace['ciudades'][$i]."' , '".$ciudad_enlace['enlaces'][$i]."' )") or die("Problemas en el select".mysqli_error($conexion));
-	
-		$page3 = new Page();
-		$page3->setUrl("https://www.tripadvisor.com.ve".$ciudad_enlace['enlaces'][$i]);
-		$builder3 = new PageBuilder($page3);
-		$builder3->setDataConfig([
+		
+		$cadena = $ciudad_enlace['enlaces'][$i];
+		$patron = "/Reviews-/";
+		
+		$encontrado = preg_match_all($patron, $cadena, $coincidencias, PREG_OFFSET_CAPTURE);
+
+		if ($encontrado) {
 		    
-		    'nombre' => "/html/body[@id='BODY_BLOCK_JQUERY_REFLOW']/div[@id='PAGE']/div[@id='MAINWRAP']/div[@id='MAIN']/div[@id='BODYCON']/div[@class='wrpHeader']/div[@class='ui_container page']/div[@class='ui_columns listingsAndFilters']/div[@id='VR_SRP_CENTER_COLUMN']/div[3]/div[@class='vr_listings']/div[@class='vr_listing']/div/div/div/a",
+		    foreach ($coincidencias[0] as $coincide) {
+		        /*print "<p>Cadena: '$coincide[0]' - Posición: $coincide[1]</p>\n";
+		        echo "<br>";*/
 
-		]);
+		        $cadena_1 = substr($cadena,1,$coincide[1]-1+strlen($coincide[0]));
+		        $cadena_2 = substr($cadena,($coincide[1]-1+strlen($coincide[0]))+1,strlen($cadena));
 
-		$director2 = new PageBuilderDirector($builder3);
-		$director2->buildPage();
-		$hoteles = $page3->getData();
-		/*echo "<pre>";
-		print_r ($hoteles);
-		echo "</pre>";*/
-		
-		echo $ciudad_enlace['ciudades'][$i];		
-		$ciudad_id = $i + 1;
-		
-		for ($x=0; $x < count($hoteles['nombre']); $x++) { 
-			mysqli_query($conexion,"insert into hoteles (nombre, ciudad_id) values 
-		                       ('".$hoteles['nombre'][$x]."' , '".$ciudad_id."' )") or die("Problemas en el select".mysqli_error($conexion));
-		}
-		
-		/*echo "<pre>";
-		print_r ($hoteles);
-		echo "</pre>";*/
+                $sw = "0";
+		        $w = 0;
+		        for ($y=0; $y <= $iterador ; $y++) { 
+		        	$ww = "";
+		        	if ($sw == "0") {
+		        		$ww= $cadena_1 . $cadena_2;
+		        	}else  {
+		        		$ww= $cadena_1 . "oa" . $w . "-" . $cadena_2;
+		        	}
+		            $w = $w + 50;
+		        	$sw = "1";
+		        	echo $ww . "<br>";
+		        	echo "https://www.tripadvisor.com.ve".$ww;
+
+	                    $url2 = "https://www.tripadvisor.com.ve".$ww;
+		                $page3 = new Page();
+				      	$page3->setUrl("https://www.tripadvisor.com.ve/".$ww);
+						$builder3 = new PageBuilder($page3);
+						$builder3->setDataConfig([
+						    
+						    'nombre' => "/html/body[@id='BODY_BLOCK_JQUERY_REFLOW']/div[@id='PAGE']/div[@id='MAINWRAP']/div[@id='MAIN']/div[@id='BODYCON']/div[@class='wrpHeader']/div[@class='ui_container page']/div[@class='ui_columns listingsAndFilters']/div[@id='VR_SRP_CENTER_COLUMN']/div[3]/div[@class='vr_listings']/div[@class='vr_listing']/div/div/div/a",
+
+						]);
+
+						$director2 = new PageBuilderDirector($builder3);
+						$director2->buildPage();
+						$hoteles = $page3->getData();
+						$ciudad_id = $i + 1;
+
+						for ($x=0; $x < count($hoteles['nombre']); $x++) { 
+							$nombre_hotel = preg_replace('/\'/', ' ', $hoteles['nombre'][$x]);
+							mysqli_query($conexion,"insert into hoteles (nombre, ciudad_id) values 
+						                       ('".$nombre_hotel."' , '".$ciudad_id."' )") or die("Problemas en el select".mysqli_error($conexion));
+						}
+					
+			        }
+			    }
+			} else {
+			    print "<p>No se han encontrado coincidencias.</p>\n";
+			}
+
 	}
 			
 
@@ -74,7 +113,5 @@
 	print_r ($hoteles);
 	echo "</pre>";
 		
-	
-	
 
 	mysqli_close($conexion);	
--- aplication/scrapper2.php
+++ aplication/scrapper2.php
@@ -41,17 +41,13 @@
 	echo "<pre>";
 	print_r ($ciudad_enlace);
 	echo "</pre>";
-	$j = 0;
+	$j = 0;	
 	
-	
-
 	for ($i=0; $i < 3; $i++) { 
 
 		$ciudad = $ciudad_enlace['ciudades'][$i]; 
 		$iterador = floor ( preg_replace("/[^0-9]/", "", $ciudad)/50 )-1;  
-		echo "Iterador :". $iterador . "<br>";
-		//echo  $iterador = floor (  preg_replace("/[^0-9]/", "", $ciudad)/50 ); 
-
+		/*echo "Iterador :". $iterador . "<br>";*/
 		mysqli_query($conexion,"insert into ciudades (nombre, enlace) values 
 		                       ('".$ciudad_enlace['ciudades'][$i]."' , '".$ciudad_enlace['enlaces'][$i]."' )") or die("Problemas en el select".mysqli_error($conexion));
 		
@@ -63,11 +59,9 @@
 		if ($encontrado) {
 		    
 		    foreach ($coincidencias[0] as $coincide) {
-		        print "<p>Cadena: '$coincide[0]' - Posición: $coincide[1]</p>\n";
-		        //echo " Cadena :".$coincidencias[0];
-		        echo "<br>";
+		        /*print "<p>Cadena: '$coincide[0]' - Posición: $coincide[1]</p>\n";
+		        echo "<br>";*/
 
-		        
 		        $cadena_1 = substr($cadena,1,$coincide[1]-1+strlen($coincide[0]));
 		        $cadena_2 = substr($cadena,($coincide[1]-1+strlen($coincide[0]))+1,strlen($cadena));
 
@@ -83,12 +77,8 @@
 		            $w = $w + 50;
 		        	$sw = "1";
 		        	echo $ww . "<br>";
-                    //----------------------------------
 		        	echo "https://www.tripadvisor.com.ve".$ww;
-		        	//$w = "";
-                    //----------------------------------
 
-                    // if ($y==0) {
 	                    $url2 = "https://www.tripadvisor.com.ve".$ww;
 		                $page3 = new Page();
 				      	$page3->setUrl("https://www.tripadvisor.com.ve/".$ww);
@@ -109,75 +99,19 @@
 							mysqli_query($conexion,"insert into hoteles (nombre, ciudad_id) values 
 						                       ('".$nombre_hotel."' , '".$ciudad_id."' )") or die("Problemas en el select".mysqli_error($conexion));
 						}
-					// }
 					
-                    //----------------------------------
-
 			        }
 			    }
 			} else {
 			    print "<p>No se han encontrado coincidencias.</p>\n";
 			}
 
-    //             $page3 = new Page();
-		  //     	$page3->setUrl("https://www.tripadvisor.com.ve".$ciudad_enlace['enlaces'][$i]);
-				// $builder3 = new PageBuilder($page3);
-				// $builder3->setDataConfig([
-				    
-				//     'nombre' => "/html/body[@id='BODY_BLOCK_JQUERY_REFLOW']/div[@id='PAGE']/div[@id='MAINWRAP']/div[@id='MAIN']/div[@id='BODYCON']/div[@class='wrpHeader']/div[@class='ui_container page']/div[@class='ui_columns listingsAndFilters']/div[@id='VR_SRP_CENTER_COLUMN']/div[3]/div[@class='vr_listings']/div[@class='vr_listing']/div/div/div/a",
-
-				// ]);
-
-				// $director2 = new PageBuilderDirector($builder3);
-				// $director2->buildPage();
-				// $hoteles = $page3->getData();
-				
-				
-				//echo $ciudad_enlace['ciudades'][$i];		
-				//$ciudad_id = $i + 1;
-		
-
-		       /* echo "Cadena 1 :" . $cadena_1  . "<br>";
-		        echo "Cadena 2 :" . $cadena_2  . "<br>";*/
-		        //x = valor/50
-		        
-		
-	
-		/*$page3 = new Page();
-		$page3->setUrl("https://www.tripadvisor.com.ve".$ciudad_enlace['enlaces'][$i]);
-		$builder3 = new PageBuilder($page3);
-		$builder3->setDataConfig([
-		    
-		    'nombre' => "/html/body[@id='BODY_BLOCK_JQUERY_REFLOW']/div[@id='PAGE']/div[@id='MAINWRAP']/div[@id='MAIN']/div[@id='BODYCON']/div[@class='wrpHeader']/div[@class='ui_container page']/div[@class='ui_columns listingsAndFilters']/div[@id='VR_SRP_CENTER_COLUMN']/div[3]/div[@class='vr_listings']/div[@class='vr_listing']/div/div/div/a",
-
-		]);
-
-		$director2 = new PageBuilderDirector($builder3);
-		$director2->buildPage();
-		$hoteles = $page3->getData();
-		
-		
-		echo $ciudad_enlace['ciudades'][$i];		
-		$ciudad_id = $i + 1;
-		
-
-		//codigo de prueba
-		for ($x=0; $x < count($hoteles['nombre']); $x++) { 
-			mysqli_query($conexion,"insert into hoteles (nombre, ciudad_id) values 
-		                       ('".$hoteles['nombre'][$x]."' , '".$ciudad_id."' )") or die("Problemas en el select".mysqli_error($conexion));
-		}*/
-		
-		/*echo "<pre>";
-		print_r ($hoteles);
-		echo "</pre>";*/
 	}
 			
 
 	echo "<pre>";
-	//print_r ($hoteles);
+	print_r ($hoteles);
 	echo "</pre>";
 		
-	
-	
 
 	mysqli_close($conexion);	
